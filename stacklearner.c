1ac0cdecd5d6f3a61a3a9c859f817619914baf2f
mujibur <mujibur777888@gmail.com>
2020-12-11 15:29:07 -0800

    add a c file for test please testing my file

---
+++ stacklearner.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   printf("^^^^^^^^^^^^^^^^^^^^^^^^^ \n");
+   printf("good morning sir \n");
+   printf("have a nice day  \n");
+   printf("welcome to stack learnar \n");
+   printf("what i can do fo you sir \n");
+   printf("########################## \n");
+   return 0;
+}
